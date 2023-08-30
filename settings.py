4fd85ddcb3bf0fae4ff4213882f3335ddbe4efbe
vetripy <vetrisurabi@gmail.com>
2023-08-31 03:24:57 +0530

    allowed hosts

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@c)*dkuq+arph^m$+v*k!ay(0t5=*7@mxw2(&33599t_g(hnvg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
