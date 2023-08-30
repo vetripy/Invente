a9703f68ea80682e460613e49bce05de7d6187d4
vetripy <vetrisurabi@gmail.com>
2023-08-31 03:18:16 +0530

    backend

---
+++ InventePT/cusform/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
+# Create your models here.
+#create user model
+class User(AbstractUser):
+    pass
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    choices = (('team', 'Team'), ('solo', 'Solo'), ('both', 'Team/Solo'))
+    team_type = models.CharField(max_length=4, choices=choices)
+    team_size = models.IntegerField(MinValueValidator(0))
+    depts = (
+        ('cse', 'Computer Science'),
+        ('ece', 'ECE'),
+        ('eee', 'EEE'),
+        ('mech', 'Mechanical'),
+        ('civil', 'Civil'),
+        ('chem', 'Chemical'),
+        ('it', 'Information Technology'),
+        ('bme', 'Biomedical Engineering')
+    )
+    dept = models.CharField(max_length=5, choices=depts)
+    rounds = models.IntegerField(validators=[MinValueValidator(0)])
+    venue = models.CharField(max_length=100)
+    date = models.DateField()
+    first_prize = models.IntegerField(MinValueValidator(0))
+    second_prize = models.IntegerField(MinValueValidator(0))
+    third_prize = models.IntegerField(MinValueValidator(0))
+
+    def __str__(self):
+        return self.title
+    
+class Round(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='all_rounds')
+
+    def __str__(self):
+        return "{0} - {1}".format(self.event.title, self.title)
