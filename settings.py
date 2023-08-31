bba3a8f681a08d5992ebdd23b777d3115f31ffa3
vetripy <vetrisurabi@gmail.com>
2023-08-31 18:39:06 +0530

    changes

--- settings.py
+++ settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@c)*dkuq+arph^m$+v*k!ay(0t5=*7@mxw2(&33599t_g(hnvg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
